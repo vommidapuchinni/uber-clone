f8fe272af4ff10ab9174d2c6a610afc58300d7bd
root <root@ip-172-31-19-92.ec2.internal>
2024-07-19 07:16:24 +0000

    2nd

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "madhu-vcube1" # Replace with your actual S3 bucket name
+    bucket = "uberclone-bucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
